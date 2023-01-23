597e5450b56ca4287d26d53324427d997da8003d
tomasphn <combomeal12@gmail.com>
2023-01-22 17:14:47 -0800

    change reaction_count method

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,4 +1,6 @@
 class Reaction < ApplicationRecord
   belongs_to :response
   belongs_to :user
+
+  validates :emotion, presence: true, inclusion: { in: %w(love funny sad happy) }
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -3,17 +3,9 @@ class Response < ApplicationRecord
   belongs_to :user
   has_many :reactions, dependent: :destroy
 
-  class << self
-    def all_and_reactions
-      Response.find_each.map do |response|
-        attributes = response.attributes
-        attributes["reactions"] = emotion_count(response[:id])
-        attributes
-      end
-    end
-
-    def emotion_count(id)
-      Response.joins(:reactions).where("responses.id = #{id}").group('reactions.emotion').count
-    end
+  def reaction_count
+    reactions.map do |reaction|
+      reaction.emotion
+    end.tally
   end
 end
