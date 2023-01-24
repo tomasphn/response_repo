8d0a103bcbb53ccfb6e5da10351d8a8c552ae4f4
tomasphn <combomeal12@gmail.com>
2023-01-24 15:44:58 -0800

    add validation for 1 reaction of each type per user per response

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -3,4 +3,5 @@ class Reaction < ApplicationRecord
   belongs_to :user
 
   validates :emotion, presence: true, inclusion: { in: %w(likes loves haha wow sad angry) }
+  validates_uniqueness_of :emotion, :response_id, :user_id, scope: [:emotion, :response_id, :user_id]
 end
