535648f988a31d758c705aa996ea9a877f13ca5e
kuanchiliao1 <kuanchiliao@gmail.com>
2023-02-01 20:30:13 -0500

    Integrate AI output with rest of code

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -5,13 +5,7 @@ class Prompt < ApplicationRecord
   EMOTIONS = %w(likes loves haha wow sad angry)
 
   def generate_responses_reactions
-    generated_responses = [
-      {responseText: "My favorite movie is [The Shawshank Redemption], I love the story, the characters and the ending.", likes: 5, loves: 2, haha: 0, wow: 3, sad: 0, angry: 0},
-      {responseText: "I can't pick just one, but I love [The Godfather], [The Godfather Part II], and [The Godfather Part III] they are all amazing movies.", likes: 4, loves: 1, haha: 0, wow: 3, sad: 0, angry: 0},
-      {responseText: "I adore the movie [The Dark Knight], I love the story, the characters and the acting, it's a masterpiece.", likes: 2, loves: 6, haha: 0, wow: 3, sad: 0, angry: 0},
-      {responseText: "One of my favorite movies is [Schindler's List], it's a powerful and moving film, it tells a very important story.", likes: 3, loves: 0, haha: 0, wow: 2, sad: 0, angry: 0},
-      {responseText: "I am a huge fan of [Forrest Gump], it's a heartwarming and funny film, I love the story, the characters and the acting.", likes: 5, loves: 1, haha: 0, wow: 3, sad: 0, angry: 0}
-      ]
+    generated_responses = gtp3_responses[1..-1]
 
     generated_responses.each do |gen_response|
       response_hash = {text: gen_response[:responseText], user_id: SEED_USERS.sample}
@@ -28,14 +22,15 @@ class Prompt < ApplicationRecord
     end
   end
 
-  # private
-
-
-  def self.gtp3_responses
-    client = OpenAI::Client.new(access_token: "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")
+  def gtp3_responses
+    file_path = Rails.root.join("app", "models", "api_key.txt")
+    file = File.open(file_path, "r")
+    api_key = file.read
+    file.close
+    client = OpenAI::Client.new(access_token: api_key)
 
     # Still need to find a way to get user input
-    user_input = "Hey, I'm feeling a little down today."
+    user_input = text
 
     # This is added to the prompt the user enters
     final_input = <<~HEREDOC
@@ -43,9 +38,10 @@ class Prompt < ApplicationRecord
 
     Examples:
 
-    Input: "What is your favorite hobby?"
+    Input: "wat ur fav hobby?"
     Output:
     [
+    {correctText: "What is your favorite hobby"},
     {responseText: "My favorite hobby is reading, I love getting lost in a good book and escaping into different worlds.", likes: 5, loves: 2, haha: 0, wow: 3, sad: 0, angry: 0},
     {responseText: "I love painting and drawing, it's a great way for me to express myself and be creative.", likes: 4, loves: 1, haha: 0, wow: 3, sad: 0, angry: 0},
     {responseText: "My hobby is playing music, it brings me so much joy and it's a great way to relax and unwind.", likes: 2, loves: 6, haha: 0, wow: 3, sad: 0, angry: 0},
@@ -73,9 +69,9 @@ class Prompt < ApplicationRecord
               prompt: final_input,
               max_tokens: 1000
           })
-    # response["choices"].map do |a|
-    #   a["text"]
-    # end
+    response["choices"].map do |a|
+      a["text"]
+    end
 
   response_array = response["choices"].map {|a| a["text"]}
   parsed_array = eval(response_array.first)
